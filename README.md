92e8277889606eac302ce30e936832235f8bc078
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-02-24 20:21:56 +0000

    Added helper classes

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # readme-generator-nodejs
 Creates a readme file based on questions
+
+# Bootcamp Challenge 11 - Readme Generator
+
+## Description 
+
+This project was created using node.js with the fs and inquirer modules to make a command-line tool that generates a HTML portfolio page generator. The project had the following requirements:
+
+In this activity, you will build a command-line tool that generates an HTML portfolio page from user input.
+
+## Instructions
+
+* Your application should prompt the user for information like their name, location, bio, LinkedIn URL, and GitHub URL. Feel free to add any additional prompts you think of.
+
+* An HTML document containing the information collected from the prompts should be constructed and written to the file system. Be sure to add some CSS styling to the document.
+
+* You’ll need the following tools and technologies to accomplish this:
+
+  * `fs` for writing to the file system.
+
+  * `inquirer` for collecting user input.
+
+  * String template literals for generating a string version of the HTML document before it is written to the file system.
+
+  * Promises for handling asynchronous behavior.
+
+
+The page in this repo is the result.
+
+## Installation
+
+The site does not require installation steps.
+
+## Usage 
+
+The website can be viewed at [https://bowseruk.github.io/weather-dashboard/](https://bowseruk.github.io/weather-dashboard/) with Google Chrome. The source can be viewed in Chrome by right clicking and selecting inspect.
+
+![Screenshot of the Webpage](assets/images/screenshot.png)
+
+## Credits
+
+* This site was based on a project by edX Boot Camps LLC.
+
+* [W3School](https://www.w3schools.com/) was used as a reference for elements to use and good practice.
+
+* The changes were checked with [W3C Validator](https://validator.w3.org/).
+
+* [Stack Overflow](https://stackoverflow.com/) always seems to have the answer to a problem that occurs.
+
+* [MDN](https://developer.mozilla.org/en-US/) is a very comprehensive and useful resource.
+
+* [Font Awesome]() was used and the documentation referenced.
+
+* [JQuery](https://api.jquery.com/) documentation was referred to throughout the project.
+
+* [Day.js](https://day.js.org/en/) was used, and the documentation was referred to throughout the project.
+
+* [5 Day Weather Forecast](https://openweathermap.org/forecast5) API was used for the project.
+
+## License
+
+This project uses the licence in the LICENCE file of the repo.
--- index.js
+++ index.js
@@ -1,21 +1,136 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// using newer syntax
+import fs from "fs";
+import path from 'path';
+// latest version of inquirer
+import inquirer from 'inquirer';
+// import generateMarkdown from "utils/generateMarkdown";
+
+// The class is a helper for questions in inquierer
+class Question {
+    constructor(type, name, message, choices = []) {
+        this.type = type;
+        this.name = name;
+        this.message = message;
+        if (choices.length > 0) {
+            this.choices = choices;
+        }
+    }
+}
+// This class makes the readme
+class ReadmeObject {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description
+        this._descriptionList = ["test1", "test2", "test3"];
+        this._license = true;
+    };
+    get descriptionList() {
+        if (this._descriptionList) {
+            return this._descriptionList.map(key => `\n- ${key}`).join("\n");
+        }
+        return "";
+    };
+    // This makes the first section with the title and description. And a list of requirements if it has been added.
+    get descriptionSection() {  
+        return `# ${this.title}\n\n## Description\n\n${this.description}\n${this.descriptionList}`;
+    }
+    // This makes the contents section
+    get contents() {
+        if (this._contents) {
+            return "\n\n## Table of Contents\n\n- Description\n\n- Contents";
+        }
+        return "";
+    }
+    // This makes the installation section
+    get installation() {
+        return "";
+    }
+    // This makes the usage section
+    get usage() {
+        return "";
+    }
+    // This makes the credits section
+    get credits(){
+        return "";
+    }
+    // This makes the features section
+    get features() {
+        return "";
+    }
+    // This makes the contribute section
+    get contribute() {
+        return "";
+    }
+    // This makes the licence section
+    get license() {
+        if (this._license) {
+            return `\n\n## License\n\nThis project uses the license in the LICENSE file of the repo.`;
+        }
+        return "";
+    }
+    get readme() {
+        return this.descriptionSection +
+        this.contents +
+        this.installation +
+        this.usage +
+        this.credits +
+        this.features +
+        this.contribute +
+        this.license
+// `
+// ## Installation
+
+// ${this.installation}
+
+// ## Usage
+
+// ${this.usage}
+
+// ${this.screenshot}
+
+// ## Credits
+
+// ## Features
+        
+// If your project has a lot of features, list them here.
+        
+// ## How to Contribute${this.license}
+// `
+    }
+}
 
 // array of questions for user
 const questions = [
-
+    new Question("input", "title", "What is the project called?"),
+    new Question("input", "description", "Please describe the project:")
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            ...questions
+        ])
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+            let readme = new ReadmeObject(answers.title, answers.description)
+            console.log(readme.readme);
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
 }
 
 // function call to initialize program
 init();
+
